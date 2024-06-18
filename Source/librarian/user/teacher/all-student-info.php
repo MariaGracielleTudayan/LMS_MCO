2ccc5762a3f674a3e5a372dd06ac39e41c5f0ffa
MARIA GRACIELLE TUDAYAN <145459165+MariaGracielleTudayan@users.noreply.github.com>
2024-06-18 06:03:46 -0400

    Add files via upload

---
+++ Source/librarian/user/teacher/all-student-info.php
@@ -0,0 +1,49 @@
+	<?php 
+		include 'inc/header.php';
+	 ?>
+	<!--dashboard area-->
+	<div class="dashboard-content">
+		<div class="dashboard-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="left">
+							<p><span>dashboard</span>Control panel</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="right text-right">
+							<a href="dashboard.php"><i class="fas fa-home"></i>home</a>
+							<span class="disabled">all teacher info</span>
+						</div>
+					</div>
+				</div>
+				<div class="student-wrapper">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="std-info">
+								<table class="table  table-striped table-dark text-center">
+									<thead>
+										<tr>
+											<th>Id No</th>
+											<th>Name</th>
+											<th>Username</th>
+											<th>Deparment</th>
+											<th>Email</th>
+											<th>Phone</th>
+											<th>Address</th>
+										
+										</tr>
+									</thead>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>					
+		</div>
+	</div>
+	<?php 
+		include 'inc/footer.php';
+	 ?>		
